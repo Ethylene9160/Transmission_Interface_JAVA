d0af0de041726802362873000c02a06a8318f35c
吉安 季 <1120939526@qq.com>
2023-11-23 23:08:35 +0800

    update the comments.

--- src/web_tools/WebUtil.java
+++ src/web_tools/WebUtil.java
@@ -4,6 +4,10 @@ import java.io.Closeable;
 import java.io.IOException;
 
 public class WebUtil {
+    /**
+     * Close all the instances of Closeable you pass.
+     * @param cs The instances of Closeable you want to close.
+     */
     public static void closeAll(Closeable...cs)  {
         for (Closeable c : cs) {
             try {
